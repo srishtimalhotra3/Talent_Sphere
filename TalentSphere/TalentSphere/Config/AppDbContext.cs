65948a231ef7c7748bba277c95df232ad12f4b80
Kumar <2478593@cognizant.com>
2026-03-09 14:55:10 +0530

    first commit

---
+++ TalentSphere/TalentSphere/Config/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TalentSphere.Config
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
