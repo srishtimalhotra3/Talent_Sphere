655aa861608109aa981b03081e4f84600cf060b7
Kumar <2478593@cognizant.com>
2026-03-16 14:25:43 +0530

    Added ConfigurationMapperProfile.cs and Interface folder

---
+++ TalentSphere/TalentSphere/DTOs/CreateComplianceRecordDTO.cs
@@ -0,0 +1,17 @@
+﻿using TalentSphere.Enums;
+
+namespace TalentSphere.DTOs
+{
+    public class CreateComplianceRecordDTO
+    {
+        public int EmployeeID { get; set; }
+
+        public CompilanceRecordType Type { get; set; }
+
+        public string Result { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
